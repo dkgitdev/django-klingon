ddebc060a3bc7f0ddc9839e21df49ba286ea868a
Dmitry Arkhipenko <dk.dev@pm.me>
2018-09-29 01:57:27 +0700

    Fix: Django 2.0 compatibility (GenericForeignKey)

--- klingon/models.py
+++ klingon/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django import urls as urlresolvers
 from django.core.cache import cache
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.generic import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 from django.utils.translation import ugettext as _
 
